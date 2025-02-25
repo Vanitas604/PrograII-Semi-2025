109ae3643bab43dd2b969b361d61c4b406aeaafe
Jennifer_baires <jennferbaires@gmail.com>
2025-02-25 10:01:05 -0600

    Código de Parcial I

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -25,15 +25,47 @@ import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tempVal;
+    TabHost tbh;
+
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tbh = findViewById(R.id.tbhMetros);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("Metros").setContent(R.id.tabMetros).setIndicator("Metros", null));
+        tbh.addTab(tbh.newTabSpec("Area").setContent(R.id.tabConversor).setIndicator("Area", null));
 
 
+        btn = findViewById(R.id.btnMetrosConvertir);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tempVal = findViewById(R.id.txtMetrosCantidad);
+                double num1 = Double.parseDouble(tempVal.getText().toString());
+
+
+                double Calculo = 0;
+
+                if(num1 <= 18 ){
+                    Calculo = 6;
+                }else if (num1 <=28){
+                    Calculo = (num1 - 18) * 0.45 + 6;
+                }else if (num1 >=29) {
+                    Calculo = (num1 - 28) * 0.65 + 4.5 + 6;
+
+
+                }
+                tempVal = findViewById(R.id.lblrespuesta);
+                tempVal.setText("Precio: "+ Calculo +"$");
+            }
+        });
     }
-
 }
-
 
 
